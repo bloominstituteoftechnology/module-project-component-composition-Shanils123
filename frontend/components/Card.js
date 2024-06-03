74631e8d2a945bf81ee84da2a8df417158c0cafa
Shanil Sanchez <shanils857@gmail.com>
2024-06-03 16:13:37 -0400

    module 4 completed

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,9 +1,28 @@
 import React from "react";
 import Figure from './Figure'
+import styled from "styled-components";
+
+const StyledCard = styled.div`
+    padding: 2rem;
+    border-radius: 12px;
+
+    h2 {
+        font-size: 2.5em;
+        margin: 0 0 1rem 0;
+    }
+
+    p {
+        margin: 0 0 1.5rem 0;
+
+        &::first-letter {
+            font-size: 1.5rem;
+        }
+    }
+`
 
 export default function Card({title, text, imageURL, date}){
     return (
-    <div className='card'>
+    <StyledCard className='card'>
         <h2 className="title">{title}</h2>
         <p className="text">{text}</p>
         
@@ -11,6 +30,6 @@ export default function Card({title, text, imageURL, date}){
         imageURL = {imageURL}
         caption = {date}
     />
-    </div>
+    </StyledCard>
     )
 }
--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,5 +1,17 @@
 import React from "react";
+import styled from "styled-components";
 
+const StyledFigure = styled.figure `
+    max-width: 50%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    figcaption{
+        font-style: italic;
+    }
+`
 
 export default function Figure({imageURL, caption}){
     return (
